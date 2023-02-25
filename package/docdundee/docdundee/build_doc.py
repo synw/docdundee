282ee5eba9c7e0afe49f976ef65fe66d5669a9d9
synw <synwx@protonmail.com>
2023-02-25 17:36:52 +0100

    Fix in python package

--- package/docdundee/docdundee/build_doc.py
+++ package/docdundee/docdundee/build_doc.py
@@ -9,11 +9,11 @@ def parse_funcs():
     alt = parse_functions("docdundee.docparser")
     doc = parse_docstrings(alt)
     # print(doc)
-    write_docstrings("../../docsite/public/doc/python_api/docstrings.json", doc, 4)
+    write_docstrings("../../docsite/public/doc/python/api/docstrings.json", doc, 4)
 
     inter = parse_functions("docdundee.interfaces")
     doc = parse_docstrings(inter, exec_examples=True)
-    write_docstrings("../../docsite/public/doc/python_types/docstrings.json", doc, 4)
+    write_docstrings("../../docsite/public/doc/python/types/docstrings.json", doc, 4)
 
 
 def main():
